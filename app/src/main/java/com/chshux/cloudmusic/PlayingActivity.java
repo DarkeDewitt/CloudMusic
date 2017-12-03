60d6d4b4544734e8b5a10d34d6b1114d340173fc
DarkeDewitt <chshux@gmail.com>
2017-12-04 03:28:29 +0800

    增加了动画，修改了一下界面

--- app/src/main/java/com/chshux/cloudmusic/PlayingActivity.java
+++ app/src/main/java/com/chshux/cloudmusic/PlayingActivity.java
@@ -1,10 +1,16 @@
 package com.chshux.cloudmusic;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class PlayingActivity extends AppCompatActivity {
 
@@ -16,11 +22,42 @@ public class PlayingActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("囍帖街");
+        getSupportActionBar().setSubtitle("Eason");
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        SeekBar seekBar = findViewById(R.id.volumeBar);
+        final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setMax(maxVolume * 10);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setProgress(currentVolume * 10);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress / 10, 0);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+        ImageView disc = findViewById(R.id.disc);
+        ImageView cover = findViewById(R.id.songcover);
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(disc, "rotation", 0f, 360f);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(16000)
+                .setRepeatCount(-1);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(cover, "rotation", 0f, 360f);
+        animator2.setInterpolator(new LinearInterpolator());
+        animator2.setDuration(16000)
+                 .setRepeatCount(-1);
+        animatorSet.play(animator).with(animator2);
+        animatorSet.start();
     }
 }
